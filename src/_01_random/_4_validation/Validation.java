a2fbdaddf99f5b8f3b6c690b75ace57413798e98
league <league@imac-20.attlocal.net>
2022-12-28 16:56:02 -0800

    Today I finished Dice Roll and started on Validation.

--- src/_01_random/_4_validation/Validation.java
+++ src/_01_random/_4_validation/Validation.java
@@ -15,9 +15,12 @@ public class Validation {
 		int randomNumber = randomMaker.nextInt(5);
 		
 		System.out.println(randomNumber);
-
+		Random ran = new Random();
+		int randomChoice = ran.nextInt(5) + 1;
 		// 1. Use each value of randomNumber to give the user a random compliment.
-
+		 if (randomChoice == 0) {
+		 }JOptionPane.showInputDialog("You're sleighin' it!");{
+			  }JOptionPane.showInputDialog("You're awwww-some!!!");
 		// 2. Repeat all the code above 10 times
 		
 		// 3. Find someone to test out your program. They will like it :)
