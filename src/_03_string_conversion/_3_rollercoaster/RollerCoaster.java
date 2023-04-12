cc8f17f296f8afeb9a4b6602e746aa0052b60906
jeremycui2008 <jeremycuid@gmail.com>
2023-04-12 16:58:06 -0700

    Today I've completed Strings Cannot Add and worked on Roller Coaster.

--- src/_03_string_conversion/_3_rollercoaster/RollerCoaster.java
+++ src/_03_string_conversion/_3_rollercoaster/RollerCoaster.java
@@ -5,11 +5,11 @@ import javax.swing.JOptionPane;
 public class RollerCoaster {
 
 	// 1) Make a main method that includes all the steps below…. 
-		
+		  public static void main (String[] args ) {
 		// 2) Ask the user to enter their height in inches using JOptionPane and set it to the variable heightToRide
 		
 		// Uncomment the line below...
-			//if( heightToRide > 42 ) { }
+			if( heightToRide > 42 ) { }
 		
 		// Do you see the heightToRide > 42 underlined red? That is because you are comparing a STRING to an INTEGER.
 		
@@ -21,5 +21,7 @@ public class RollerCoaster {
 		// 5) Else if the heightAsInt is greater than or equal to 42 inches tall print:
 		// "You can ride the coaster with someone else!" 
 	
-		// 6) else print: "You must be at least 42 inches tall to ride the roller coaster pal!"
+		// 6) else print: "You must be at least 42 inches tall to ride the roller coaster, pal!"
 } 
+		  
+}
