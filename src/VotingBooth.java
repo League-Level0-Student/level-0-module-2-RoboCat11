7a600c58da80acbbd365f48e47fa5820c0d1ca95
jeremycui2008 <jeremycuid@gmail.com>
2023-04-19 17:00:08 -0700

    Today I worked on Roller Coaster and Voting Booth.

---
+++ src/VotingBooth.java
@@ -0,0 +1,20 @@
+import javax.swing.JOptionPane;
+public class VotingBooth {
+public static void main(String[] args) {
+	
+
+String input = JOptionPane.showInputDialog("How old are you (in years)?");
+int age = Integer.parseInt(input);
+if(age>18) {
+
+String nextPresident = JOptionPane.showInputDialog("Who do you think the next president should be?");	
+}
+if() {
+JOptionPane.showMessageDialog(null,"Nobody cares what you think!");	
+}
+}
+}
+
+
+
+
