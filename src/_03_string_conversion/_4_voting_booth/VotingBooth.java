8526899bb0b18edbd7368df26e1490e5d4b5a7a0
league <alishahussain1229@gmail.com>
2023-04-29 11:26:13 -0700

    Today I finished Voting Booth and Fix The Random.

--- src/_03_string_conversion/_4_voting_booth/VotingBooth.java
+++ src/_03_string_conversion/_4_voting_booth/VotingBooth.java
@@ -1,3 +1,4 @@
+package _03_string_conversion._4_voting_booth;
 import javax.swing.JOptionPane;
 public class VotingBooth {
 public static void main(String[] args) {
@@ -9,7 +10,7 @@ if(age>18) {
 
 String nextPresident = JOptionPane.showInputDialog("Who do you think the next president should be?");	
 }
-if() {
+if(age<=18) {
 JOptionPane.showMessageDialog(null,"Nobody cares what you think!");	
 }
 }
--- src/_04_spot_the_error/HowMany.java
+++ src/_04_spot_the_error/HowMany.java
@@ -9,7 +9,7 @@ public class HowMany {
 	// Change one line of code to fix this program.
 	
 	public static void main(String[] args) {
-		int number = 0;
+		int number = 1;
 		System.out.println(number);
 	}
 	
