ae03269fa7c2d11c8504bb03b822a89541ed4a84
league <alishahussain1229@gmail.com>
2023-06-07 16:56:42 -0700

    Today I finished Soundtrack to Life and Star Show.

--- src/_99_extra/StarShow.java
+++ src/_99_extra/StarShow.java
@@ -14,41 +14,42 @@ public class StarShow {
 	
 	void makeStars() {
 		
-		drawStar(150);	//5. delete this line. you will draw the star again in step 8.
-		// 13. Set the speed to 8
-
-		// 6. Make a variable to hold the X position of the Robot and set it to 10
-
-		// 7. Make a variable to hold the Y position of the Robot and set it to 600
-
-		// 8. Make a variable to hold the star size and set it to 25
-
-		// 12. Repeat the steps #19 to #18, 30 times
-
-			// 19. Set the pen width to i 
-	
-			// 10. Set the X position of the robot to your X variable
-	
-			// 11. Set the Y position of the robot to your Y variable
-	
-			// 9. Call the drawStar() method with your star size variable
-	
-			// 14. Increase the X position by star size. See Figure 2.
-	
-			// 15. decrease the Y position by star size. See Figure 3.
-	
-			// 16. Increase the star size by 20
-	
-			// 17. Turn the robot 12 degrees
-	
-			// 18. Make each star a different random color like in Figure 4. 
-
+		//5. delete this line. you will draw the star again in step 8.
+		
+	   // 13. Set the speed to 8
+	   rob.setSpeed(8);
+	   // 6. Make a variable to hold the X position of the Robot and set it to 10
+	   int x = 10;
+	   // 7. Make a variable to hold the Y position of the Robot and set it to 600
+	   int y = 600;
+	   // 8. Make a variable to hold the star size and set it to 25
+	   int size = 25;
+	   // 12. Repeat the steps #19 to #18, 30 times
+	   for(int i = 0; i>30; i++) {	
+       // 19. Set the pen width to i 
+	   rob.setPenWidth(i);
+	   // 10. Set the X position of the robot to your X variable
+	   rob.setX(x);
+	   // 11. Set the Y position of the robot to your Y variable
+	   rob.setY(y);;
+	   // 9. Call the drawStar() method with your star size variable
+	   drawStar(size);
+	   // 14. Increase the X position by star size. See Figure 2.
+	   x = x + size;
+	   // 15. decrease the Y position by star size. See Figure 3.
+	   y = y - size;
+	   // 16. Increase the star size by 20
+	   size = size + 20;
+	   // 17. Turn the robot 12 degrees
+	   rob.turn(12);
+	   // 18. Make each star a different random color like in Figure 4. 
+	   rob.setRandomPenColor();
 	}
-
+}
 	private void drawStar(int starSize) {
 		
 
-			  
+			for(int i = 0; i>5; i ++){  
 			// 1. Move the robot the distance of the starSize variable
 				rob.move(starSize);
 			// 2. Put the robot's pen down
@@ -56,10 +57,8 @@ public class StarShow {
 			// 3. Turn the robot 144 degrees
 				rob.turn(144);
 		   // 4. Repeat both commands 5 times. See Figure 1 at http://bit.ly/star-show
-				for(i) {
+				}
 				
-				}
-				}
 }	
 	public static void main(String[] args) {
 		new StarShow().makeStars();
